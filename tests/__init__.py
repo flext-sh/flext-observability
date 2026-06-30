2a5fce379ef7a5bfd6c6f74da51f6de418ef3274
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-06-29 22:39:01 -0300

    Regenerate lazy wrapper exports

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,12 +3,64 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from flext_core.lazy import (
     build_lazy_import_map,
     install_lazy_exports,
     merge_lazy_imports,
 )
 
+if TYPE_CHECKING:
+    from flext_tests import (
+        d as d,
+        e as e,
+        h as h,
+        r as r,
+        td as td,
+        tf as tf,
+        tk as tk,
+        tm as tm,
+        tv as tv,
+        x as x,
+    )
+
+    from tests.base import (
+        TestsFlextObservabilityServiceBase as TestsFlextObservabilityServiceBase,
+        s as s,
+    )
+    from tests.constants import (
+        TestsFlextObservabilityConstants as TestsFlextObservabilityConstants,
+        c as c,
+    )
+    from tests.models import (
+        TestsFlextObservabilityModels as TestsFlextObservabilityModels,
+        m as m,
+    )
+    from tests.protocols import (
+        TestsFlextObservabilityProtocols as TestsFlextObservabilityProtocols,
+        p as p,
+    )
+    from tests.settings import (
+        TestsFlextObservabilitySettings as TestsFlextObservabilitySettings,
+    )
+    from tests.typings import (
+        TestsFlextObservabilityTypes as TestsFlextObservabilityTypes,
+        t as t,
+    )
+    from tests.unit.test_constants import (
+        TestsFlextObservabilityConstantsUnit as TestsFlextObservabilityConstantsUnit,
+    )
+    from tests.unit.test_factory import (
+        TestsFlextObservabilityFactory as TestsFlextObservabilityFactory,
+    )
+    from tests.unit.test_init import (
+        TestsFlextObservabilityInit as TestsFlextObservabilityInit,
+    )
+    from tests.utilities import (
+        TestsFlextObservabilityUtilities as TestsFlextObservabilityUtilities,
+        u as u,
+    )
 _LAZY_IMPORTS = merge_lazy_imports(
     (
         ".integration",
