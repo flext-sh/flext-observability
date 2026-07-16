d20fc54c3bbec9d08e04843940931f223e4eb8a4
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-16 10:18:44 -0300

    fixes

--- src/flext_observability/protocols.py
+++ src/flext_observability/protocols.py
@@ -13,7 +13,7 @@ from collections.abc import (
 from typing import Protocol, runtime_checkable
 
 from flext_cli import p
-from flext_observability import p, t
+from flext_observability import t
 
 
 class FlextObservabilityProtocols(p):
